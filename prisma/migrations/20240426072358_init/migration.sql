619792da171ed7f71125fcff2ce49c87b498bd1e
jackcaca <jackcaca0519@yahoo.com.tw>
2024-04-26 17:45:09 +0800

    change database

--- prisma/migrations/20240426072358_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "people" INTEGER NOT NULL DEFAULT 0
-);
