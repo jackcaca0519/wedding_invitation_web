def3a209b77cde6475be8190ae229e1e99204c93
jackcaca <jackcaca0519@yahoo.com.tw>
2024-11-07 17:57:09 +0800

    adjust content

--- app/success/page.js
+++ app/success/page.js
@@ -22,7 +22,7 @@ const SuccessPage = async () => {
                             width={150}
                             height={150}/>
                     </div>
-                    <Card className='p-3' style={{position:'relative', height: 500, boxShadow: '0 0 20px 0px #c5c5c5', backgroundImage: "url('/img/carousel-1.jpg')", backgroundPosition: 'center',backgroundSize: 'cover'}}>
+                    <Card className='p-3' style={{position:'relative', height: 500, boxShadow: '0 0 20px 0px #c5c5c5', backgroundImage: "url('/img/custom/107_5360.jpg')", backgroundPosition: 'center',backgroundSize: 'cover'}}>
                         <div className="h-100 d-flex justify-content-center align-items-center" style={{background: '#00000070', backdropFilter: 'blur(3px)', borderRadius: 3,}}>
                             <div className='w-100 d-flex flex-column align-items-center pt-4' style={{position: 'absolute', top: 0}}>
                                 <CheckCircleOutlineIcon style={{color: 'white', fontSize: '50px'}}></CheckCircleOutlineIcon>
@@ -35,7 +35,7 @@ const SuccessPage = async () => {
                                     </div>
                                     <div className="py-1 px-md-4 d-flex justify-content-center">
                                         <div className="text-left">
-                                            <h4 className="text-uppercase font-weight-normal m-0 text-white" style={{letterSpacing: 2}}>時間：2025.02.22 17:00</h4>
+                                            <h4 className="text-uppercase font-weight-normal m-0 text-white" style={{letterSpacing: 2}}>時間：2025.02.22 17:30</h4>
                                             <h4 className="text-uppercase font-weight-normal m-0 text-white" style={{letterSpacing: 2}}>地點：老新臺菜十全店</h4>
                                         </div>
                                     </div>
